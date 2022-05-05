30cfe097e4035dc644e9e17b92cc752e6bcd6b73
Aldin <adnzain40@gmail.com>
2022-05-05 08:20:44 +0700

    make profile image upload system

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,8 @@
 <?php 
 // isi nama host, username mysql, dan password mysql anda
-$conn = mysqli_connect("localhost","root","","db_oprec");
+$conn = mysqli_connect("localhost","aldin","password","db_oprec");
 if (!$conn) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
     die("Koneksi gagal: " . mysqli_connect_error());
 };
 ?>
